a1e950ec1670d717dbc2ddbbaae8e77010f54380
Jpschnabel177 <jpschnabel96@gmail.com>
2022-12-22 11:25:17 -0500

    wasted time on implemented unnecesary feature

--- src/components/projectcard.jsx
+++ src/components/projectcard.jsx
@@ -3,26 +3,35 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
-
+import ThumbsUpButton from "./thumbsup";
 
 const ProjectCard = (props) => {
-    return (
-      <>
-        <div className="game-card">
+  return (
+    <>
+      <div className="game-card">
+        <Link to={`/projects/${props.singleProject._id}`}>
+          <img className="imgcard" src="/public/vite.svg" alt="" />
+        </Link>
+        {/* <h1>{props.singleProject.owner.displayName}</h1> */}
+        <div className="game-card-bottom">
           <Link to={`/projects/${props.singleProject._id}`}>
-            <img className="imgcard" src="/public/vite.svg" alt="" />
+            <h4 style={{ marginTop: "0", marginBottom: "5px" }}>
+              {props.singleProject.title}
+            </h4>
           </Link>
-          {/* <h1>{props.singleProject.owner.displayName}</h1> */}
-          <div className="game-card-bottom">
-            <Link to={`/projects/${props.singleProject._id}`}>
-              <h4 style={{ marginTop: "0", marginBottom: "5px" }}>
-                {props.singleProject.title}
-              </h4>
-            </Link>
-            <p style={{ height: "90px" }}>
-              {props.singleProject.description.short}
-            </p>
-            <div style={{display:"flex", flexWrap: "wrap", justifyContent: "space-evenly"}}>
+          <p style={{ height: "90px" }}>
+            {props.singleProject.description.short}
+          </p>
+          <div style={{display: "flex", alignItems: "center", paddingLeft: "5px", paddingRight: "5px"}}>
+            <div><ThumbsUpButton singleProject={props.singleProject}/></div>
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "center",
+                height: "100%",
+              }}
+            >
               {props.singleProject.tech.engines.map((element) => {
                 return <div className="skill-tag"> {element} </div>;
               })}
@@ -32,7 +41,8 @@ const ProjectCard = (props) => {
             </div>
           </div>
         </div>
-      </>
-    );
-}
-export default ProjectCard
+      </div>
+    </>
+  );
+};
+export default ProjectCard;
---
+++ src/components/thumbsup.jsx
@@ -0,0 +1,106 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+
+function ThumbsUpButton(props) {
+  const { user, isLoggedIn, logOutUser } = useContext(AuthContext);
+  //let userVotedProject = user.upvoted.find(props.singleProject._id);
+
+  let userVoted;
+
+
+
+  const [filled, setFilled] = useState(userVoted);
+
+  const [projectUpvotes, setProjectUpVotes] = useState(
+    props.singleProject.upvotes
+  );
+
+  function handleClick() {
+    setFilled(!filled);
+    if (filled) {
+      axios
+        .put(
+          `http://localhost:3001/api/projects/${props.singleProject._id}/upvote`,
+          {
+            upvotes: projectUpvotes + 1,
+          },
+          {
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("authToken")}`,
+            },
+          }
+        )
+        .then(() => {
+          setProjectUpVotes(projectUpvotes + 1);
+          axios.put(
+            `http://localhost:3001/api/user/addupvoted/${props.singleProject._id}`,
+            {
+              headers: {
+                authorization: `Bearer ${localStorage.getItem("authToken")}`,
+              },
+            }
+          );
+        });
+    } else {
+      axios
+        .put(
+          `http://localhost:3001/api/projects/${props.singleProject._id}/upvote`,
+          {
+            upvotes: projectUpvotes - 1,
+          },
+          {
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("authToken")}`,
+            },
+          }
+        )
+        .then(() => {
+          setProjectUpVotes(projectUpvotes - 1);
+          axios.put(
+            `http://localhost:3001/api/user/deleteupvoted/${props.singleProject._id}`,
+            {
+              headers: {
+                authorization: `Bearer ${localStorage.getItem("authToken")}`,
+              },
+            }
+          );
+        });
+    }
+  }
+
+  return (
+    <div>
+      {/* {filled ? (
+        <button
+          className="upvoteButton"
+          style={{ backgroundColor: "#007bff" }}
+          onClick={handleClick}
+        >
+          <img
+            src="/public/upvote.png"
+            className="upvoteimg"
+            style={{ height: "35px", width: "35px" }}
+          />
+        </button>
+      ) : (
+        <button
+          className="upvoteButton"
+          style={{
+            backgroundColor: "transparent",
+          }}
+          onClick={handleClick}
+        >
+          <img
+            src="/public/upvote.png"
+            className="upvoteimg"
+            style={{ height: "35px", width: "35px" }}
+          />
+        </button>
+      )} */}
+    </div>
+  );
+}
+
+export default ThumbsUpButton;
